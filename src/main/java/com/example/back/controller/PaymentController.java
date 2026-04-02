c39251d32865b7da869736cf105730c54e41c0a2
Jayanth122006 <jayanthptnl06@gmail.com>
2026-04-02 14:46:46 +0530

    fix cashfree verification

--- src/main/java/com/example/back/controller/PaymentController.java
+++ src/main/java/com/example/back/controller/PaymentController.java
@@ -54,7 +54,7 @@ public class PaymentController {
         
         JSONObject requestBody = new JSONObject();
         requestBody.put("order_id", orderId);
-        requestBody.put("order_amount", 100.0);
+        requestBody.put("order_amount", 1.0);
         requestBody.put("order_currency", "INR");
         
         JSONObject customerDetails = new JSONObject();
